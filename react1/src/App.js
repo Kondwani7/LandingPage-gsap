2ddcb54a11facf4ba2e7db323ddb90ab5a834332
Kondwani7 <kondwanigideon.ngulube@gmail.com>
2022-06-13 09:53:15 +0200

    react animation

--- react1/src/App.js
+++ react1/src/App.js
@@ -1,12 +1,43 @@
+import React, {useRef, useEffect} from 'react'
 import logo from './logo.svg';
 import './App.css';
-
+import {TweenMax, Power3} from 'gsap'
 function App() {
+  let logoItem = useRef(null);
+  let editText = useRef(null);
+
+  useEffect(() => {
+    console.log(editText);
+    TweenMax.to(
+      logoItem,
+      .8,
+      {
+        opacity: 1,
+        y: -20,
+        ease: Power3.easeOut,
+        delay:.3
+      }
+    )
+    TweenMax.to(
+      editText,
+      .8,
+      {
+        opacity: 1,
+        y:-20,
+        ease: Power3.easeOut,
+        delay:.6
+      }
+    )
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <img
+          ref={el => {logoItem=el}}
+          src={logo} className="App-logo" alt="logo" />
+        <p
+          ref={el => {editText=el}}
+        >
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
