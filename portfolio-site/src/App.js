9951e8594315aa5b1fa2666fdf903313db839bd5
Kondwani7 <kondwanigideon.ngulube@gmail.com>
2022-06-14 20:35:07 +0200

    layout

--- portfolio-site/src/App.js
+++ portfolio-site/src/App.js
@@ -1,10 +1,23 @@
 import './App.scss';
-import {Routes} from 'react-router-dom'
+import {Route, Routes} from 'react-router-dom'
+import './components/Layout'
+import Layout from './components/Layout';
+import Home from './components/Home'
+import Contact from './components/Contact'
+import Portfolio from './components/Portfolio'
+import About from './components/About'
 function App() {
   return (
-    <div className="App">
-    
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+        </Route>
+      </Routes>
+    </>
   );
 }
 
--- portfolio-site/src/components/Layout/index.js
+++ portfolio-site/src/components/Layout/index.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 import './index.scss'
+import Sidebar from '../Sidebar'
 
 const Layout = () => {
   return (
-    <div>layout</div>
+    <div className='App'>
+      <Sidebar/>
+      <div className='page'>
+        <span className='tags top-tags'>&lt;body&gt;</span>
+        <Outlet/>
+        <span>
+          &lt;/body&gt;
+          <br/>
+          <span className='bottom-tag-html'>&lt;/html&gt;</span>
+        </span>
+      </div>
+    </div>
   )
 }
 
