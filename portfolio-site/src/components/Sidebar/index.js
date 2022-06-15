50f529872faf539b6fecf1ed3486f89a2a37ac48
Kondwani7 <kondwanigideon.ngulube@gmail.com>
2022-06-15 12:11:29 +0200

    sidebar

--- portfolio-site/src/components/Sidebar/index.js
+++ portfolio-site/src/components/Sidebar/index.js
@@ -3,7 +3,10 @@ import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {BsLinkedin, BsGithub,BsYoutube, BsSkype} from 'react-icons/bs'
-import {AiFillFacebook} from 'react-icons/ai'
+import {AiFillFacebook, AiFillHome} from 'react-icons/ai'
+import {FaUserAlt, FaSuitcase} from 'react-icons/fa'
+import {MdEmail} from 'react-icons/md'
+import {FcContacts} from 'react-icons/fc'
 import './index.scss'
 import {Link, NavLink} from 'react-router-dom'
 const Sidebar = () => {
@@ -13,6 +16,62 @@ const Sidebar = () => {
         <img src={LogoS} alt="logo"/>
         <img src={LogoSubtitle} alt="slobodoan"/>
       </Link>
+      <nav>
+        <NavLink exact="true" activeclassname="active" to="/">
+          <AiFillHome color="#4d4d4e"/>
+        </NavLink>
+        <NavLink activeclassname="active" className="about-link" to="/about">
+          <FaUserAlt color="#4d4d4e"/>
+        </NavLink>
+        <NavLink activeclassname="active" className="portfolio-link" to="/portfolio">
+          <FaSuitcase color="#4d4d4e"/>
+        </NavLink>
+        <NavLink
+          activeclassname="active"
+          className="contact-link"
+          to="/contact"
+        >
+          <FcContacts color="#4d4d4e"/>
+        </NavLink>
+      </nav>
+      <ul>
+        <li>
+          <a
+            href='https://www.linkedin.com/in/kondwani-ngulube-4489ba174/'
+            target='blank'
+            rel='noreferrer'
+          >
+            <BsLinkedin color="#4d4d4e"/>
+          </a>
+        </li>
+        <li>
+          <a
+            href='https://github.com/kondwani7'
+            target='blank'
+            rel='noreferrer'
+          >
+            <BsGithub color="#4d4d4e"/>
+          </a>
+        </li>
+        <li>
+          <a
+            href=''
+            target='blank'
+            rel='noreferrer'
+          >
+            <BsYoutube color="#4d4d4e"/>
+          </a>
+        </li>
+        <li>
+          <a
+            href=''
+            target='blank'
+            rel='noreferrer'
+          >
+            <BsSkype color="#4d4d4e"/>
+          </a>
+        </li>
+      </ul>
     </div> 
   )
 }
