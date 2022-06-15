9e3cc0d027912db0b9e4f72f57c05de2b6a7f01d
Kondwani7 <kondwanigideon.ngulube@gmail.com>
2022-06-15 13:43:19 +0200

    logo gsap animation

--- portfolio-site/src/components/Home/Logo/index.js
+++ portfolio-site/src/components/Home/Logo/index.js
@@ -12,6 +12,28 @@ const Logo = () => {
   const solidLogoRef = useRef();
 
   useEffect(() => {
+    gsap.registerPlugin(DrawSVGPlugin)
+    /*helps use adjust the opacity of a specific element in a html tag in  */
+    gsap.timeline().to(bgRef.current, {
+      duration: 1,
+      opacity: 1
+    })
+    .from(outlineLogoRef.current, {
+      drawSVG:0,
+      duration: 20
+    })
+    
+    gsap.fromTo(
+      solidLogoRef.current,
+      {
+        opacity: 0
+      },
+      {
+        opacity: 1,
+        delay: 4,
+        duration: 4
+      }
+    )
 
   },[])
   return (
